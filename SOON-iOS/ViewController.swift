7a2db61ecd5ba61e5456ed732c21f79fafbd73f1
yuji-nakamoto <yuji0411@icloud.com>
2022-04-13 11:21:44 +0900

    Initial Commit

---
+++ SOON-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOON-iOS
+//
+//  Created by yuji nakamoto on 2022/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
